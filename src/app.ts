04883ae167e77c8c6a4839abd93d429071d6409b
rafael <rafaelciprianosilva0@gmail.com>
2025-07-18 15:49:30 -0300

    feat: handle application errors or handled in the code
    Handles errors thrown by me through the AppError class, validation errors using ZodError and
    application errors.

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import { routes } from "@/routes/index.routes"
+import { errorHandler } from "@/middleware/errorHandler"
 
 const app = express()
 
 app.use(express.json())
 app.use(routes)
+
+app.use(errorHandler)
 
 export { app }
---
+++ src/middleware/errorHandler.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from "express";
+import { AppError } from "@/utils/AppError";
+import { z, ZodError } from "zod";
+
+export function errorHandler(
+  error: any,
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  if(error instanceof AppError) {
+    return response.status(error.statusCode).json({ message: error.message })
+  }
+
+  if(error instanceof ZodError) {
+    return response.status(400).json({
+      message: "validation error",
+      issues: z.treeifyError(error)
+    })
+  }
+
+  return response.status(500).json({ message: error.message })
+}
