cf68d37f1d5389e2235bfb66df7f1513a3a3c3fb
rafael <rafaelciprianosilva0@gmail.com>
2025-07-19 15:03:14 -0300

    feat: allow creating a new user

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,8 +1,37 @@
 import { Request, Response } from "express";
+import { z } from "zod";
+import { hash } from "bcrypt"
+import { prisma } from "@/database/prisma";
+import { AppError } from "@/utils/AppError";
 
 class UserController {
-  create(request: Request, response: Response) {
-    return response.status(201).json()
+  async create(request: Request, response: Response) {
+    const requestData = z.object({
+      name: z.string().trim().min(3),
+      email: z.email(),
+      password: z.string().min(6)
+    }).catchall(z.string())
+
+    const { name, email, password } = requestData.parse(request.body)
+
+    const encryptedPassword = await hash(password, 8)
+
+    const emailIsAlreadyInUse = await prisma.users.findFirst({ where: { email } })
+
+    if(emailIsAlreadyInUse)
+      throw new AppError("Email is already in use.")
+
+    const user = await prisma.users.create({
+      data: {
+        name,
+        email,
+        password: encryptedPassword
+      }
+    })
+
+    const { password: _, ...userDataWithoutPassword } = user
+
+    return response.status(201).json(userDataWithoutPassword)
   }
 }
 
